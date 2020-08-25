25367f0f3c9faf96d93c767776e459ef206da095
Anton <semenyakin.anton@gmail.com>
2020-08-25 14:16:42 +0300

    deleting duplicate code "repositories..."

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,10 +21,6 @@ allprojects {
     }
 }
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
